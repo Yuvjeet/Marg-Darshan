c57bf126c9877e85dd37bc3992639cec5f1fb40f
Yuvjeet <artiarorasingh@gmail.com>
2023-11-20 20:44:35 +0530

    Solve the API Local host Issue

--- static/JS/app.js
+++ static/JS/app.js
@@ -148,7 +148,7 @@ function next(){
 }
 
 function API(){
-    fetch('http://127.0.0.1:5000/predict', {
+    fetch('/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
